044009df4ca1f8a272469b6a178553bfe23cfbd7
PoLLi <1657076+poLLi@users.noreply.github.com>
2020-07-21 08:34:48 +0200

    new mobile navigation
    added burger menu
    added menu animation
    changed 100vh calc to proper to function

--- src/assets/ts/main.ts
+++ src/assets/ts/main.ts
@@ -1,10 +1,52 @@
-console.log('It works just fine!');
+// ----------------------------------------------------------------------
+// Variables
+const burgerMenu: Element = document.querySelector('.burger');
+const navMenu: Element = document.querySelector('.nav');
+const navLinks = document.querySelectorAll('.nav li');
 
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+// ----------------------------------------------------------------------
+// EventListeners
+window.addEventListener('resize', getVH);
+burgerMenu.addEventListener('click', openMobileMenu);
 
-window.addEventListener('resize', () => {
-    // We execute the same script as before
+navLinks.forEach((item) => {
+    item.addEventListener('click', closeMobileMenu);
+});
+
+// ----------------------------------------------------------------------
+// Functions
+function getVH() {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+}
+
+function openMobileMenu() {
+    navMenu.classList.toggle('nav-open');
+    navLinkAnimate();
+}
+
+function closeMobileMenu() {
+    navMenu.classList.remove('nav-open');
+    navLinkAnimate();
+}
+
+function navLinkAnimate() {
+    // stop animation when not in mobile view
+    if (window.innerWidth <= 576) {
+        navLinks.forEach((link: HTMLElement, index: number) => {
+            if (link.style.animation) {
+                link.style.animation = ``;
+            } else {
+                link.style.animation = `navItemAnim 0.5s ease forwards ${index / 7 + 0.25}s`;
+            }
+        });
+    }
+}
+
+// App Function
+function app() {
+    // Call all initial functions
+    getVH();
+}
+
+app();
